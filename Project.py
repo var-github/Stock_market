f7ac6f467ed51c274d9a0ff24da4d08a384fd3a6
Varun R <64779815+var-github@users.noreply.github.com>
2023-03-23 22:52:04 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -22,4 +22,7 @@ from selenium import webdriver
 
 # Connecting to database and creating required tables
 mycon = connector.connect(**st.secrets["mysql"])
+db = mycon.cursor()
+db.execute("create table if not exists users (user_id int primary key, username varchar(30) not null unique, password text not null, cash float(7, 2) default(10000.00), status varchar(8) default ('ENABLED'));")
+db.execute("create table if not exists transaction (user_id int, transaction_id int primary key, symbol varchar(15) not null, shares int not null, price float(7, 2) not null, transacted char(19));")
 st.button("Hi")
