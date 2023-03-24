f6e4331965e95a8b1af6878c1fc4c51fcaad9b93
Varun R <64779815+var-github@users.noreply.github.com>
2023-03-24 17:31:02 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -3,21 +3,10 @@
 
 import mysql.connector as connector
 from st_on_hover_tabs import on_hover_tabs
-from captcha.image import ImageCaptcha
-import os
-import sys
-import random
-import yfinance
-from datetime import date, datetime, timedelta
 from streamlit_echarts import st_echarts
 import requests
 import streamlit as st
-from PIL import Image
-import pandas
-import time
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium import webdriver
 import selenium
 
@@ -39,117 +28,6 @@ st.set_page_config(
 if 'page' not in st.session_state:
     st.session_state['page'] = 1
 
-st.text("Here 1")
-if st.session_state['page'] in [1, 2, 4, 5, 6, 8]:
-    img = 'https://img.freepik.com/stock-market-forex-trading-graph-graphic-double-exposure_73426-193.jpg'
-else:
-    img = 'https://cdn.pixabay.com/photo/2018/01/12/16/16/growth-3078543_1280.png'
-
-css = f"""
-<style>
-    section[data-testid='stSidebar'] {{
-        top: -1%;
-        background-color: #111;
-        min-width:unset !important;
-        width: unset !important;
-    }}
-    /* Hide close button on the streamlit navigation menu */
-    button[kind="header"] {{
-        display: none;
-    }}
-    /* The navigation menu */
-    section[data-testid='stSidebar'] > div {{
-        height: 120%;
-        width: 75px;
-        position: relative;
-        top: 0;
-        left: 0;
-        background-color: #111;
-        transition: 0.7s ease;
-    }}
-    /* The navigation menu on hover */
-    section[data-testid='stSidebar'] > div:hover{{
-        top: -10%;
-        width: 270px;
-    }}
-    section[data-testid='stSidebar'] > div:hover img{{
-        position: relative;
-        transition: 0.7s ease;
-        max-width: 80% !important;
-        left: 10% !important;
-    }}
-    section[data-testid='stSidebar'] > div img{{
-        position: relative;
-        left: 10% !important;
-        max-width: 100% !important;
-        transition: 0.7s ease;
-    }}
-    #MainMenu {{
-        visibility: hidden;
-    }}
-    button[title='View fullscreen'], a:not(a[target=_blank]), footer {{visibility: hidden;}}
-    footer:after {{
-        content:'Made with Streamlit'; 
-        visibility: visible;
-        display: block;
-        position: relative;
-        #background-color: red;
-        padding: 3px;
-        top: 1px;
-    }}
-    .stSpinner{{
-        position: fixed;
-        width: 40%;
-        top: 45%;
-        left: 45%;
-    }}
-    [data-testid="stAppViewContainer"] > .main {{
-        background-image: url("{img}");
-        background-repeat: no-repeat;
-        background-size: 120%;
-    }}
-    [data-testid="stHeader"] {{
-        display: none;
-    }}
-    #welcome {{
-        color: lightyellow;
-        font-size:50px;
-        border-radius:2%
-    }}
-    table {{
-        background-color: rgba(255, 255, 255, 0.3);
-       -webkit-backdrop-filter: blur(4px);
-       backdrop-filter: blur(4px);
-    }}
-    table > tbody > tr:nth-of-type(1) > td {{
-        font-weight: bold;
-        text-align: center! important;
-        font-family: "Times New Roman", Times, serif;
-    }}
-    table > tbody > tr:nth-of-type(1) {{
-        background: lightgrey;
-    }}
-    td {{
-        text-align: center! important;
-    }}
-    th {{
-        display: none;
-    }}
-    hr {{
-        background-color: #00000040;
-    }}
-    iframe[title="streamlit_echarts.st_echarts"] {{
-        position: relative;
-        left: 185px;
-        width: 800px;
-        border: 1.5px solid;
-        background: rgba(255, 255, 255, 0.62);
-    }}
-    [data-testid="stForm"] {{border: 0px}}
-</style>
-"""
-st.markdown(css, unsafe_allow_html=True)
-
 
 # Function to check for internet connectivity
 def internet():
@@ -158,21 +36,6 @@ def internet():
         return True
     except:
         return False
-
-# Getting USD - INR conversion rates
-def usd():
-    if 'usd' not in st.session_state or not st.session_state['usd']:
-        if internet():
-            url = f'https://api.exchangerate.host/timeseries?base={"USD"}&start_date={date.today()}&end_date={date.today()}&symbols={"INR"}'
-            data = requests.get(url).text
-            st.session_state['usd'] = float(data[data.rfind('INR') + 5:-3])
-            return st.session_state['usd']
-        else:
-            st.session_state['usd'] = ""
-    else:
-        return st.session_state['usd']
-usd()
-
 
 # Initializing variables
 if 'user' not in st.session_state:
@@ -183,30 +46,6 @@ if 'user' not in st.session_state:
     driver.get('https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en')
     driver.implicitly_wait(2)
     st.session_state['driver'] = driver
-if 'captcha' not in st.session_state:
-    st.session_state['captcha'] = ""
-if 'clicked' not in st.session_state:
-    st.session_state['clicked'] = False
-if 'company' not in st.session_state:
-    st.session_state['company'] = ""
-if 'shares' not in st.session_state:
-    st.session_state['shares'] = 1
-if 'successful' not in st.session_state:
-    st.session_state['successful'] = ""
-if 'warning' not in st.session_state:
-    st.session_state['warning'] = 0
-if 'stock' not in st.session_state:
-    st.session_state['stock'] = "<select>"
-if 'agree' not in st.session_state:
-    st.session_state['agree'] = False
-if 'name' not in st.session_state:
-    st.session_state['name'] = ""
-if 'username' not in st.session_state:
-    st.session_state['username'] = ""
-if 'password' not in st.session_state:
-    st.session_state['password'] = ""
-if 'confirm' not in st.session_state:
-    st.session_state['confirm'] = ""
 
 
 # The page is divided into 3 columns - the first column has only back button, column 2 has the rest of the data
