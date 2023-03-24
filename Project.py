251a04095e8f52e78f2d37ecdf494f1003958b71
Varun R <64779815+var-github@users.noreply.github.com>
2023-03-24 16:22:24 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -596,111 +596,114 @@ def transactions():
 
 # MAIN CODE
 # Every page is a separete if block, the variable - st.session_state['page'] - moniters which page is currently displayed
-if st.session_state['page'] == 1:
-    column1, column2, column3 = st.columns([1, 4.5, 1])
-    home_page = """
-    <p style="position: absolute; color: lightblack; font-size:24px; left: 15.4%; padding-right:15.4%">
-        <b>
-            Welcome to Omni-Stocks, a website equiped with a verstalie real-life platform for 
-            comencing investments. The money being utilised is NOT REAL and has no effect on the 
-            actual market. Our website allows users to invest in both NASDAQ and NSE. A wide range 
-            of real-time data and analytics is available on our website. Enjoy your experience
-        </b>
-    </p>
-    """
-    st.markdown(home_page, unsafe_allow_html=True)
-    st.sidebar.image("https://app.omnistock.io/uploads/logo/yktS4FqNbQGn3TychVaEzDIkHoiJa4Ei5HPSAIAy.png")
-    st.sidebar.caption("")
-    st.sidebar.caption("")
-    column2.header("WELCOME", anchor="welcome")
-    with st.sidebar:
-        current_tab = on_hover_tabs(tabName=['Home','Login', 'Register', 'Admin','Contact Us','Help'], iconName=['home','account_circle','economy','dashboard','phone','help_center'], styles = {'tabOptionsStyle': {':hover :hover': {'color': 'blue'}}}, key=105, default_choice=0)
-        if current_tab =='Login':
-            st.session_state['page'] = 6
-            st.experimental_rerun()
-        elif current_tab == 'Register':
-            st.session_state['page'] = 8
-            st.experimental_rerun()
-        elif current_tab == 'Admin':
-            st.session_state['page'] = 2
-            st.experimental_rerun()
-        elif current_tab == 'Help':
-            st.session_state['page'] = 5
-            st.experimental_rerun()
-
-
-# Admin login page
-elif st.session_state['page'] == 2:
-    column2.header("ADMIN")
-    column1.text("")
-    if column1.button(label="🔙"):
-        st.session_state['page'] = 1
-        st.experimental_rerun()
-    form = column2.form(key="admin_login")
-    username = form.text_input('Username')
-    password = form.text_input('Password', type="password")
-    if form.form_submit_button(label="Login"):
-        db.execute("SELECT username, password from users where user_id = 1;")
-        data = db.fetchall()
-        if username != data[0][0]:
-            column2.warning('Wrong username')
-            st.stop()
-        if password != data[0][1]:
-            column2.warning("Wrong password")
-            st.stop()
-        st.session_state['page'] = 3
-        # st.experimental_rerun()
-
-
-# Admin page
-elif st.session_state['page'] == 3:
-    column1, column2, column3 = st.columns([1, 4.5, 1])
-    st.sidebar.image("https://app.omnistock.io/uploads/logo/yktS4FqNbQGn3TychVaEzDIkHoiJa4Ei5HPSAIAy.png")
-    st.sidebar.caption("")
-    st.sidebar.caption("")
-    main = st.empty()
-    table = st.empty()
-    with st.sidebar:
-        current_tab = on_hover_tabs(tabName=['View Users','Transactions', 'Logout'], iconName=['group','credit_card','logout'], styles = {'tabOptionsStyle': {':hover :hover': {'color': 'blue'}}}, key=101, default_choice=0)
-        if current_tab == "View Users":
-            users = []
-            db.execute("select * from users where not user_id = 1;")
-            data = db.fetchall()
-            for i in data:
-                users += [i[1]]
-            col1, col2, col3, col4 = main.columns([1.15, 3, 1, 1.1])
-            col2.header("Admin")
-            disable = col2.multiselect('Select users to disable / enable:', users)
-            for i in range(7):
-                col3.text("")
-            if col3.button(label="DISABLE / ENABLE"):
-                for i in disable:
-                    db.execute(f'select status from users where username = "{i}"')
-                    if db.fetchall()[0][0] == "ENABLED":
-                        db.execute(f'update users set status = "DISABLED" where username = "{i}"')
-                    else:
-                        db.execute(f'update users set status = "ENABLED" where username = "{i}"')
-                mycon.commit()
+def main():
+    if st.session_state['page'] == 1:
+        column1, column2, column3 = st.columns([1, 4.5, 1])
+        home_page = """
+        <p style="position: absolute; color: lightblack; font-size:24px; left: 15.4%; padding-right:15.4%">
+            <b>
+                Welcome to Omni-Stocks, a website equiped with a verstalie real-life platform for 
+                comencing investments. The money being utilised is NOT REAL and has no effect on the 
+                actual market. Our website allows users to invest in both NASDAQ and NSE. A wide range 
+                of real-time data and analytics is available on our website. Enjoy your experience
+            </b>
+        </p>
+        """
+        st.markdown(home_page, unsafe_allow_html=True)
+        st.sidebar.image("https://app.omnistock.io/uploads/logo/yktS4FqNbQGn3TychVaEzDIkHoiJa4Ei5HPSAIAy.png")
+        st.sidebar.caption("")
+        st.sidebar.caption("")
+        column2.header("WELCOME", anchor="welcome")
+        with st.sidebar:
+            current_tab = on_hover_tabs(tabName=['Home','Login', 'Register', 'Admin','Contact Us','Help'], iconName=['home','account_circle','economy','dashboard','phone','help_center'], styles = {'tabOptionsStyle': {':hover :hover': {'color': 'blue'}}}, key=105, default_choice=0)
+            if current_tab =='Login':
+                st.session_state['page'] = 6
                 st.experimental_rerun()
-            data = [("User ID", "Username", "Password", "Cash", "Status")] + data
-            col5, col6, col7 = table.columns([1, 3.5, 1])
-            col6.table(data)
-        elif current_tab == 'Transactions':
-            column2.header("Admin")
-            users = []
-            db.execute("select user_id, username, transaction_id, symbol, shares, price, transacted from transaction natural join users order by username, transacted desc;")
-            data = db.fetchall()
-            for i in data:
-                if i[1] not in users:
-                    users += [i[1]]
-            selected = column2.multiselect('Filter by user:', users)
-            if selected:
-                select = str(tuple(selected))[:str(selected).rfind("'") + 1] + ")"
-                db.execute(f"select user_id, username, transaction_id, symbol, shares, price, transacted from transaction natural join users where username in {select} order by transacted desc;")
-                data = db.fetchall()
-            data = [("User ID", "Username", "Transaction ID", "Symbol", "Shares", "Price", "Date")] + data
-            column2.table(data)
-        elif current_tab == 'Logout':
+            elif current_tab == 'Register':
+                st.session_state['page'] = 8
+                st.experimental_rerun()
+            elif current_tab == 'Admin':
+                st.session_state['page'] = 2
+                main()
+                return
+            elif current_tab == 'Help':
+                st.session_state['page'] = 5
+                st.experimental_rerun()
+
+
+    # Admin login page
+    elif st.session_state['page'] == 2:
+        column2.header("ADMIN")
+        column1.text("")
+        if column1.button(label="🔙"):
             st.session_state['page'] = 1
             st.experimental_rerun()
+        form = column2.form(key="admin_login")
+        username = form.text_input('Username')
+        password = form.text_input('Password', type="password")
+        if form.form_submit_button(label="Login"):
+            db.execute("SELECT username, password from users where user_id = 1;")
+            data = db.fetchall()
+            if username != data[0][0]:
+                column2.warning('Wrong username')
+                st.stop()
+            if password != data[0][1]:
+                column2.warning("Wrong password")
+                st.stop()
+            st.session_state['page'] = 3
+            main()
+            return
+
+
+    # Admin page
+    elif st.session_state['page'] == 3:
+        column1, column2, column3 = st.columns([1, 4.5, 1])
+        st.sidebar.image("https://app.omnistock.io/uploads/logo/yktS4FqNbQGn3TychVaEzDIkHoiJa4Ei5HPSAIAy.png")
+        st.sidebar.caption("")
+        st.sidebar.caption("")
+        main = st.empty()
+        table = st.empty()
+        with st.sidebar:
+            current_tab = on_hover_tabs(tabName=['View Users','Transactions', 'Logout'], iconName=['group','credit_card','logout'], styles = {'tabOptionsStyle': {':hover :hover': {'color': 'blue'}}}, key=101, default_choice=0)
+            if current_tab == "View Users":
+                users = []
+                db.execute("select * from users where not user_id = 1;")
+                data = db.fetchall()
+                for i in data:
+                    users += [i[1]]
+                col1, col2, col3, col4 = main.columns([1.15, 3, 1, 1.1])
+                col2.header("Admin")
+                disable = col2.multiselect('Select users to disable / enable:', users)
+                for i in range(7):
+                    col3.text("")
+                if col3.button(label="DISABLE / ENABLE"):
+                    for i in disable:
+                        db.execute(f'select status from users where username = "{i}"')
+                        if db.fetchall()[0][0] == "ENABLED":
+                            db.execute(f'update users set status = "DISABLED" where username = "{i}"')
+                        else:
+                            db.execute(f'update users set status = "ENABLED" where username = "{i}"')
+                    mycon.commit()
+                    st.experimental_rerun()
+                data = [("User ID", "Username", "Password", "Cash", "Status")] + data
+                col5, col6, col7 = table.columns([1, 3.5, 1])
+                col6.table(data)
+            elif current_tab == 'Transactions':
+                column2.header("Admin")
+                users = []
+                db.execute("select user_id, username, transaction_id, symbol, shares, price, transacted from transaction natural join users order by username, transacted desc;")
+                data = db.fetchall()
+                for i in data:
+                    if i[1] not in users:
+                        users += [i[1]]
+                selected = column2.multiselect('Filter by user:', users)
+                if selected:
+                    select = str(tuple(selected))[:str(selected).rfind("'") + 1] + ")"
+                    db.execute(f"select user_id, username, transaction_id, symbol, shares, price, transacted from transaction natural join users where username in {select} order by transacted desc;")
+                    data = db.fetchall()
+                data = [("User ID", "Username", "Transaction ID", "Symbol", "Shares", "Price", "Date")] + data
+                column2.table(data)
+            elif current_tab == 'Logout':
+                st.session_state['page'] = 1
+                st.experimental_rerun()
 
