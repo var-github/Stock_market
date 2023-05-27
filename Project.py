0f4598feea5637909166b3a0a640fe998de03e00
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-27 18:11:50 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -154,6 +154,7 @@ def portfolio():
     if not data:
         column2.warning("You currently have not invested in any stocks")
         st.stop()
+    """
     with st.spinner("Loading..."):
         if internet():
             sum = 0
@@ -168,7 +169,6 @@ def portfolio():
     column2.table(data)
     if internet():
         column2.text("Total: $" + str(round(sum, ndigits=2)))
-    """
     db.execute(f"select cash from users where user_id = {st.session_state['user']};")
     cash = db.fetchall()[0][0]
     column2.text("Cash left in account: $" + str(cash))
