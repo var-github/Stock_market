12cb9928adda280e3ac6a5195aaf382e9e8ab9b7
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-27 20:13:21 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -371,7 +371,7 @@ elif st.session_state['page'] == 3:
         elif current_tab == 'Transactions':
             column2.header("Admin")
             user = []
-            data = st.session_state['db'].execute(f"select user_id, username, transaction_id, symbol, shares, price, transacted from '{transaction}' natural join users order by username, transacted desc;")
+            data = st.session_state['db'].execute(f"select user_id, username, transaction_id, symbol, shares, price, transacted from '{transaction}' natural join '{users}' order by username, transacted desc;")
             data = data.fetchall()
             for i in data:
                 if i[1] not in user:
@@ -379,7 +379,7 @@ elif st.session_state['page'] == 3:
             selected = column2.multiselect('Filter by user:', user)
             if selected:
                 select = str(tuple(selected))[:str(selected).rfind("'") + 1] + ")"
-                data = st.session_state['db'].execute(f"select user_id, username, transaction_id, symbol, shares, price, transacted from '{transaction}' natural join users where username in {select} order by transacted desc;")
+                data = st.session_state['db'].execute(f"select user_id, username, transaction_id, symbol, shares, price, transacted from '{transaction}' natural join '{users}' where username in {select} order by transacted desc;")
                 data = data.fetchall()
             data = [("User ID", "Username", "Transaction ID", "Symbol", "Shares", "Price", "Date")] + data
             column2.table(data)
