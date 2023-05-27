c9b74cbff9df907060df500c44e32d61df93d451
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-27 19:11:06 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -121,8 +121,8 @@ if 'user' not in st.session_state:
     st.session_state['user'] = None
 if 'driver' not in st.session_state:
     options = Options()
-    #options.add_argument('--disable-gpu')
-    #options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get('https://www.google.com/finance/quote/TSLA:NASDAQ?hl=en')
     driver.implicitly_wait(2)
