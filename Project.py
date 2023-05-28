902cbd28a591781d1554191ed80032bff38db4ba
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-28 22:00:58 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -9,7 +9,7 @@ from shillelagh.backends.apsw.db import connect
 from geopy.geocoders import Nominatim
 import pandas
 from selenium import webdriver
-from streamlit_echarts import st_echarts
+from streamlit_echarts import st_pyecharts
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -359,8 +359,8 @@ def quote():
                     date.append(str(i.strftime("%x")))
                     value.append(round(graph[i], ndigits=2))
             if value:
-                options = {"height": 300, "tooltip": {"trigger": 'axis'}, "xAxis": {"data": date}, "yAxis": {"type":"value", "axisLine": {"show": True }, "splitLine": {"show": False}}, "series": [{"data": value, "type": 'line'}]}
-                st_echarts(options=options)
+                options = {"height": 500, "tooltip": {"trigger": 'axis'}, "xAxis": {"data": date}, "yAxis": {"type":"value", "axisLine": {"show": True }, "splitLine": {"show": False}}, "series": [{"data": value, "type": 'line'}]}
+                st_pyecharts(options=options)
             if data['Website']:
                 col1, col2, col3 = st.columns([1, 4.5, 1])
                 col2.write("Website: [" + str(data["Website"]) + "](" + str(data["Website"]) + ")")
