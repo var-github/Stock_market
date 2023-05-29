69c72215c94291da3ca9d62c3babfe88b611c8cf
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-29 08:39:37 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -27,7 +27,6 @@ src = src[:src.find('height: str = "') + 15] + "400" + src[src.find('height: str
 f.write(src)
 f.flush()
 f.close()
-st.text(inspect.getsource(streamlit_echarts))
 sys.exit()
 
 # Configuring the page
