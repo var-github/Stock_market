ad37b77dfe568efd6ff68c7fc05309a1380dc34c
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-29 08:27:41 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -22,7 +22,7 @@ f.seek(0)
 src = str(f.read())
 f.seek(0)
 src = src[:src.find('height: str = "') + 15] + "400" + src[src.find('height: str = "') + 18:]
-f.write(bytes(src))
+f.write(bytes(src, 'utf-8'))
 sys.exit()
 
 # Configuring the page
