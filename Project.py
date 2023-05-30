aeb2043058d92d688da5942316a0c13a543f4c38
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-30 10:18:57 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -366,7 +366,7 @@ def quote():
                 st_echarts(options=options)
                 js = """
                 <script>
-                    var ifram = parent.document.getElementsByTagName("iframe")[0];
+                    var ifram = parent.document.getElementsByTagName("iframe")[1];
                     ifram.style.display = "none";
                 </script>
                 """
