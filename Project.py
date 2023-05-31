004539aefd001b0cb9211034093f1dfe332a1fef
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-31 20:09:14 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -12,7 +12,6 @@ import pandas
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from captcha.image import ImageCaptcha
