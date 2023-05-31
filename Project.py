c92266f7f7aff11a7035de87ee7c766b719c1706
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-31 08:41:47 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -366,9 +366,9 @@ def quote():
                     var iframe = parent.document.getElementsByTagName("iframe")[1];
                     iframe.style.height = "650px";
                     iframe.onload = function () {
-                        console.log(iframe.contentWindow.document)
+                        doc = iframe.contentWindow.document
                     }
-                    
+                    console.log(doc.getElementsByTagName("div"));
                 </script>
                 """
                 html(js)
