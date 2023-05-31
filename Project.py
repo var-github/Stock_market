03c101c442ff896bd148f61540555f8a471e1c1b
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-31 15:46:53 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -338,6 +338,7 @@ def quote():
     column2.title("QUOTE")
     js = """
     <script>
+        console.log(document.getElementsByClassName("css-1n543e5"));
         function execute(){
             var iframe = parent.document.getElementsByTagName("iframe")[1];
             iframe.style.height = "550px";
