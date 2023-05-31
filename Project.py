98a07cad62de9770c3348dca92bdf9f4be18f610
Varun R <64779815+var-github@users.noreply.github.com>
2023-05-31 23:33:35 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -725,14 +725,11 @@ elif st.session_state['page'] == 3:
         elif current_tab == 'Transactions':
             column2.header("Admin")
             user = []
-            def all_transactions():
-                with st.spinner("Getting all transactions..."):
-                    data = st.session_state['db'].execute(f"select user_id, username, transaction_id, symbol, shares, price, SUBSTRING(transacted,1,LENGTH(transacted)-1) from '{transaction}' natural join '{users}' order by username, transacted desc;")
-                    data = data.fetchall()
-                    for i in data:
-                        if i[1] not in user:
-                            user += [i[1]]
-            all_transactions()
+            data = st.session_state['db'].execute(f"select user_id, username, transaction_id, symbol, shares, price, SUBSTRING(transacted,1,LENGTH(transacted)-1) from '{transaction}' natural join '{users}' order by username, transacted desc;")
+            data = data.fetchall()
+            for i in data:
+                if i[1] not in user:
+                    user += [i[1]]
             selected = column2.multiselect('Filter by user:', user)
             if selected:
                 select = str(tuple(selected))[:str(selected).rfind("'") + 1] + ")"
