fdf4ff8e312e575272fee8c88a3a4dd39a95b34b
Varun R <64779815+var-github@users.noreply.github.com>
2023-06-23 19:51:41 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -29,7 +29,7 @@ st.set_page_config(
 
 user_agent = streamlit_js_eval(js_expressions='/iPhone|iPad|Android/i.test(navigator.userAgent)', key = 'UA')
 if str(user_agent) == "True" or str(user_agent) == "None":
-    streamlit_js_eval(js_expressions='parent.document.body.style.zoom = "50%"', key = 'ZM')
+    streamlit_js_eval(js_expressions='parent.document.body.style.zoom = "70%"', key = 'ZM')
     zoom = 350
 else:
     zoom = 120
