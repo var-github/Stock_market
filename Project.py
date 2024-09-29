6de03bf67023abf49598bf45894a2a3ea32c9793
Varun R <64779815+var-github@users.noreply.github.com>
2024-09-29 19:23:10 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -296,8 +296,8 @@ def portfolio():
     column2.title("PORTFOLIO")
     data = st.session_state['db'].execute(f"select symbol, sum(shares) from '{transaction}' where user_id = {st.session_state['user']} group by symbol having sum(shares) != 0;")
     data = data.fetchall()
-    cc = st.session_state['db'].execute(f"select symbol, sum(shares), sum(shares*price) from '{transaction}' where user_id = {st.session_state['user']};")
-    cc = data.fetchall()
+    cc = st.session_state['db'].execute(f"select symbol, sum(shares), sum(shares*price) from '{transaction}' where user_id = {st.session_state['user']} group by symbol having sum(shares) != 0;;")
+    cc = cc.fetchall()
     st.text(cc)
     if not data:
         column2.warning("You currently have not invested in any stocks")
